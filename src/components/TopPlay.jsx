7d9e2f053e20393926716eaa62034f847b8729ff
phyowainyunt-19 <phyowainyunt.26@gmail.com>
2022-10-21 21:10:39 +0630

    TopPlay component updated | TopChartCard added

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -13,7 +13,14 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 
 const TopPlay = () => {
-  // Project revision
+
+  // Top Chart Component
+  const TopChartCard = ({ song, i }) => (
+    <div className="w-full flex flex-row items-center hover:bg-[#4c4268] py-2 p-4 rounded-lg cursor-pointer mb-2">
+      {song.title}
+    </div>
+  )
+
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
@@ -35,8 +42,34 @@ const TopPlay = () => {
     dispatch(playPause(true));
   }
   return (
-    <div ref={divRef}>
-      
+    <div
+      ref={divRef}
+      className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col"
+    >
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white text-2xl font-bold">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more ...</p>
+          </Link>
+        </div>
+        {/* Top songs */}
+        <div className="mt-4 flex flex-col gap-1">
+          {topPlays?.map((song, i) => (
+            <TopChartCard key={song.key} i={i} song={song} />
+          ))}
+        </div>
+      </div>
+      {/* Top artists */}
+      <div className="w-full flex flex-col mt-8">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white text-2xl font-bold">Top Artists</h2>
+          <Link to="/top-artists">
+            <p className="text-gray-300 text-base cursor-pointer">See more ...</p>
+          </Link>
+        </div>
+      </div>
+      {/* Swiper Component */}
     </div>
   )
 }
